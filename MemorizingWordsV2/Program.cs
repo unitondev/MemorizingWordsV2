23620bbfdb65f05f7f60b45012056d764ba694e2
Uniton <lovrovalx@gmail.com>
2021-04-19 00:07:05 +0300

    init console app

---
+++ MemorizingWordsV2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MemorizingWordsV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
