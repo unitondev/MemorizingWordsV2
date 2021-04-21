1af66d153fac09a47180673d382bef6e09938d2a
Uniton <lovrovalx@gmail.com>
2021-04-21 04:32:06 +0300

    add IWordRepository

---
+++ MemorizingWordsV2.Application/Interfaces/Repositories/IWordRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using MemorizingWordsV2.Domain.Models;
+
+namespace MemorizingWordsV2.Application.Interfaces.Repositories
+{
+    public interface IWordRepository
+    {
+        EnglishWord GetWordWordById(int id);
+        List<EnglishWord> GetAllWords();
+        void AddWord(EnglishWord englishWord);
+        void DeleteWordById(int id);
+    }
+}
