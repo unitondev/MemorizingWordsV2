1ebc980354ed4baf92311712bf92d5cc58d7f956
Uniton <lovrovalx@gmail.com>
2021-04-22 00:39:32 +0300

    added repository implementation

---
+++ MemorizingWordsV2.Application/Interfaces/Repositories/IEnglishWordRepository.cs
@@ -0,0 +1,9 @@
+﻿using MemorizingWordsV2.Domain.Models;
+
+namespace MemorizingWordsV2.Application.Interfaces.Repositories
+{
+    public interface IEnglishWordRepository : IRepositoryAsync<EnglishWord>
+    {
+           
+    }
+}
