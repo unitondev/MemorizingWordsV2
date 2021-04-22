b632ea8408fad94720ab9a1d463750a3273baa57
Uniton <lovrovalx@gmail.com>
2021-04-22 14:35:50 +0300

    added configurations for WordsDBContext

---
+++ MemorizingWordsV2.Infrastructure/Configurations/EnglishWordConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using MemorizingWordsV2.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MemorizingWordsV2.Infrastructure.Configurations
+{
+    public class EnglishWordConfiguration : IEntityTypeConfiguration<EnglishWord>
+    {
+        public void Configure(EntityTypeBuilder<EnglishWord> entity)
+        {
+            entity.Property(e => e.Id).HasColumnName("id");
+
+            entity.Property(e => e.CreatedAt)
+                .HasPrecision(0)
+                .HasColumnName("created_at");
+
+            entity.Property(e => e.PartOfSpeechId).HasColumnName("part_of_speech_id");
+
+            entity.Property(e => e.Word)
+                .IsRequired()
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("word");
+
+            entity.HasOne(d => d.PartOfSpeech)
+                .WithMany(p => p.EnglishWords)
+                .HasForeignKey(d => d.PartOfSpeechId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("FK__EnglishWo__part___45F365D3");
+
+        }
+    }
+}
--- MemorizingWordsV2.Infrastructure/WordsDBContext.cs
+++ MemorizingWordsV2.Infrastructure/WordsDBContext.cs
@@ -1,5 +1,6 @@
 ﻿using MemorizingWordsV2.Domain;
 using MemorizingWordsV2.Domain.Models;
+using MemorizingWordsV2.Infrastructure.Configurations;
 using Microsoft.EntityFrameworkCore;
 
 #nullable disable
@@ -34,79 +35,10 @@ namespace MemorizingWordsV2.Infrastructure
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "Cyrillic_General_CI_AS");
-
-            modelBuilder.Entity<EnglishRussianWord>(entity =>
-            {
-                entity.HasKey(e => new { e.EnglishId, e.RussianId })
-                    .HasName("PK__English___599173F6506BDEBE");
-
-                entity.ToTable("English_Russian_Words");
-
-                entity.Property(e => e.EnglishId).HasColumnName("english_id");
-
-                entity.Property(e => e.RussianId).HasColumnName("russian_id");
-
-                entity.HasOne(d => d.English)
-                    .WithMany(p => p.EnglishRussianWords)
-                    .HasForeignKey(d => d.EnglishId)
-                    .OnDelete(DeleteBehavior.Cascade)
-                    .HasConstraintName("FK__English_R__engli__4AB81AF0");
-
-                entity.HasOne(d => d.Russian)
-                    .WithMany(p => p.EnglishRussianWords)
-                    .HasForeignKey(d => d.RussianId)
-                    .OnDelete(DeleteBehavior.Cascade)
-                    .HasConstraintName("FK__English_R__russi__4BAC3F29");
-
-            });
-
-            modelBuilder.Entity<EnglishWord>(entity =>
-            {
-                entity.Property(e => e.Id).HasColumnName("id");
-
-                entity.Property(e => e.CreatedAt)
-                    .HasPrecision(0)
-                    .HasColumnName("created_at");
-
-                entity.Property(e => e.PartOfSpeechId).HasColumnName("part_of_speech_id");
-
-                entity.Property(e => e.Word)
-                    .IsRequired()
-                    .HasMaxLength(50)
-                    .IsUnicode(false)
-                    .HasColumnName("word");
-
-                entity.HasOne(d => d.PartOfSpeech)
-                    .WithMany(p => p.EnglishWords)
-                    .HasForeignKey(d => d.PartOfSpeechId)
-                    .OnDelete(DeleteBehavior.SetNull)
-                    .HasConstraintName("FK__EnglishWo__part___45F365D3");
-
-            });
-
-            modelBuilder.Entity<PartOfSpeech>(entity =>
-            {
-                entity.ToTable("PartOfSpeech");
-
-                entity.Property(e => e.Id).HasColumnName("id");
-
-                entity.Property(e => e.PartName)
-                    .IsRequired()
-                    .HasMaxLength(30)
-                    .IsUnicode(false)
-                    .HasColumnName("part_name");
-            });
-
-            modelBuilder.Entity<RussianWord>(entity =>
-            {
-                entity.Property(e => e.Id).HasColumnName("id");
-
-                entity.Property(e => e.Word)
-                    .IsRequired()
-                    .HasMaxLength(50)
-                    .IsUnicode(false)
-                    .HasColumnName("word");
-            });
+            modelBuilder.ApplyConfiguration(new EnglishRussianWordConfiguration());
+            modelBuilder.ApplyConfiguration(new EnglishWordConfiguration());
+            modelBuilder.ApplyConfiguration(new PartOfSpeechConfiguration());
+            modelBuilder.ApplyConfiguration(new RussianWordConfiguration());
 
             OnModelCreatingPartial(modelBuilder);
         }
