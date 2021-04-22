48fa252b70520f007b5d3b0cde9e1a7bd8345649
Uniton <lovrovalx@gmail.com>
2021-04-22 16:48:43 +0300

    added interface(2 methods) and implementation for service

---
+++ MemorizingWordsV2.Application/Interfaces/Services/IEnglishWordService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MemorizingWordsV2.Domain.Models;
+
+namespace MemorizingWordsV2.Application.Interfaces.Services
+{
+    public interface IEnglishWordService
+    {
+        public Task<IEnumerable<EnglishWord>> GetAllEnglishWordsAsync();
+        public Task<EnglishWord> GetByIdOrDefaultAsync(int id);
+    }
+}
