29a6407883f3ce94100c187872f1f7b772e5fbc6
Uniton <lovrovalx@gmail.com>
2021-04-23 06:24:14 +0300

    added and implemented FirstOrDefaultAsync method

--- MemorizingWordsV2.Application/Interfaces/Repositories/IEnglishWordRepository.cs
+++ MemorizingWordsV2.Application/Interfaces/Repositories/IEnglishWordRepository.cs
@@ -1,9 +1,10 @@
-﻿using MemorizingWordsV2.Domain.Models;
+﻿using System.Threading.Tasks;
+using MemorizingWordsV2.Domain.Models;
 
 namespace MemorizingWordsV2.Application.Interfaces.Repositories
 {
     public interface IEnglishWordRepository : IRepositoryAsync<EnglishWord>
     {
-           
+        Task<EnglishWord> FirstOrDefaultAsync(EnglishWord englishWord);
     }
 }
