24151dcff4e28f9198df89a7e4e2afef84c0ab71
Uniton <lovrovalx@gmail.com>
2021-04-24 22:01:05 +0300

    added interface and its implement for russian

---
+++ MemorizingWordsV2.Infrastructure/Repositories/RussianWordRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MemorizingWordsV2.Application.Interfaces.Repositories;
+using MemorizingWordsV2.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace MemorizingWordsV2.Infrastructure.Repositories
+{
+    public class RussianWordRepository : RepositoryAsync<RussianWord>, IRussianWordRepository
+    {
+        private WordsDBContext _dbContext;
+        public RussianWordRepository(WordsDBContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<RussianWord> FirstOrDefaultAsync(RussianWord russianWord)
+        {
+            return await _dbContext.RussianWords.FirstOrDefaultAsync(word => word.Word == russianWord.Word);
+        }
+        
+        //TODO return just necessary field, not 4 objects 
+        public async Task<List<RussianWord>> GetRelatedWordAndPartOfSpeechAsync(RussianWord russianWord)
+        {
+            var relatedWordAndPartOfSpeech = await _dbContext.RussianWords
+                .Where(rusnWord => rusnWord.Id == russianWord.Id)
+                .Include(rusWord => rusWord.EnglishRussianWords)
+                .ThenInclude(engRusWord => engRusWord.English)
+                .ThenInclude(engWord => engWord.PartOfSpeech)
+                .ToListAsync();
+            
+            return relatedWordAndPartOfSpeech;
+        }
+    }
+}
